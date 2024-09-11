459ab813d5c4335d42ae1f58e4c1dc0586f34d0e
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-11 18:30:14 +0700

    DONE

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -7,68 +7,84 @@ const RENDER_EVENT = "RENDER_EVENT";
 const formInput = document.getElementById("form-input");
 
 function saveToStorage() {
-    localStorage.setItem("books", JSON.stringify(BOOKS));
+  localStorage.setItem("books", JSON.stringify(BOOKS));
 }
 
 formInput.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const title = formInput.elements.title.value;
-    const author = formInput.elements.author.value;
-    const deadline = formInput.elements.deadline.value;
+  const title = formInput.elements.title.value;
+  const author = formInput.elements.author.value;
+  const deadline = formInput.elements.deadline.value;
 
-    const newBook = {
-        id: +new Date(),
-        title,
-        author,
-        borrowing_date: new Date().toISOString(),
-        deadline: new Date(deadline).toISOString(),
-    };
-    BOOKS.push(newBook);
-    saveToStorage();
-    document.dispatchEvent(new Event(RENDER_EVENT));
+  const newBook = {
+    id: +new Date(),
+    title,
+    author,
+    borrowing_date: new Date().toISOString(),
+    deadline: new Date(deadline).toISOString(),
+  };
+  BOOKS.push(newBook);
+  saveToStorage();
+  document.dispatchEvent(new Event(RENDER_EVENT));
 
-    formInput.reset();
+  formInput.reset();
 });
 function deleteBook(bookId) {
-    const index = BOOKS.findIndex((book) => book.id === bookId);
-    if (index !== -1) {
-        BOOKS.splice(index, 1);
-        saveToStorage();
-        document.dispatchEvent(new Event(RENDER_EVENT));
+  Sweetalert2.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Sweetalert2.fire("Deleted!", "Your file has been deleted.", "success");
     }
+  });
+  const index = BOOKS.findIndex((book) => book.id === bookId);
+  if (index !== -1) {
+    BOOKS.splice(index, 1);
+    saveToStorage();
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
 }
-function createBookElement(bookItem) {
-    const bookElement = document.createElement("book-item");
-    bookElement.setAttribute("id", bookItem.id);
-    bookElement.setAttribute("title", bookItem.title);
-    bookElement.setAttribute("author", bookItem.author);
-    bookElement.setAttribute("deadline", bookItem.deadline);
-    bookElement.setAttribute("borrowing-date", bookItem.borrowing_date);
-    bookElement.addEventListener("book-delete", (event) => {
-        const bookId = event.detail.id;
-        deleteBook(bookId);
-    });
-    return bookElement;
+function createBookElement(bookItem, index) {
+  const bookElement = document.createElement("book-item");
+  bookElement.setAttribute("id", bookItem.id);
+  bookElement.setAttribute("title", bookItem.title);
+  bookElement.setAttribute("author", bookItem.author);
+  bookElement.setAttribute("deadline", bookItem.deadline);
+  bookElement.setAttribute("borrowing-date", bookItem.borrowing_date);
+  bookElement.setAttribute("index", index);
+  bookElement.addEventListener("book-delete", (event) => {
+    const bookId = event.detail.id;
+    deleteBook(bookId);
+  });
+  return bookElement;
 }
 
 document.addEventListener(RENDER_EVENT, function () {
-    const bookList = document.getElementById("book-lists");
+  const bookList = document.getElementById("book-lists");
 
-    bookList.innerHTML = "";
-
-    for (const bookItem of BOOKS) {
-        bookList.append(createBookElement(bookItem));
-    }
+  bookList.innerHTML = "";
+  let index = 1;
+  for (const bookItem of BOOKS) {
+    bookList.append(createBookElement(bookItem, index));
+    index++;
+  }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    formValidation();
-    if (!localStorage.getItem("books")) {
-        localStorage.setItem("books", JSON.stringify(BOOKS_DUMMY));
-        BOOKS = JSON.parse(localStorage.getItem("books"));
-    } else {
-        BOOKS = JSON.parse(localStorage.getItem("books")) || [];
-    }
-    document.dispatchEvent(new Event(RENDER_EVENT));
+  formValidation();
+  AOS.init();
+  if (!localStorage.getItem("books")) {
+    localStorage.setItem("books", JSON.stringify(BOOKS_DUMMY));
+    BOOKS = JSON.parse(localStorage.getItem("books"));
+  } else {
+    BOOKS = JSON.parse(localStorage.getItem("books")) || [];
+  }
+  document.dispatchEvent(new Event(RENDER_EVENT));
 });
